6d1952f413ccec23460dfa7700dbfda3cadf31c9
Rahim Alwer <rahim.alwer@gmail.com>
2023-04-12 16:34:05 +1000

    chore: bump deps

--- vitest.js
+++ vitest.js
@@ -1,6 +1,6 @@
 import { createVitest } from 'vitest/node';
 
-const vitest = await createVitest({
+const vitest = await createVitest('test', {
   include: [`tests/gc.test.ts`],
   globals: true,
   watch: process.argv.includes('--watch'),
